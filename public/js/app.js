5be1a58d05aa1d1abb5ae3a4958dc51c7daede98
Zainab Akkadwala <zainabakkadwala@gmail.com>
2022-09-25 13:19:08 +0530

    add cart page, auth

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+console.log('Hello from app js');
+
+/***/ }),
+
+/***/ "./resources/scss/app.scss":
+/*!*********************************!*\
+  !*** ./resources/scss/app.scss ***!
+  \*********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: \"+\" and \"-\" must be surrounded by whitespace in calculations.\n   ╷\n70 │     min-height: calc(100vh-86px);\r\n   │                           ^\n   ╵\n  resources\\scss\\app.scss 70:27  root stylesheet\n    at processResult (C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\webpack\\lib\\NormalModule.js:758:19)\n    at C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\webpack\\lib\\NormalModule.js:860:5\n    at C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\sass-loader\\dist\\index.js:54:7\n    at Function.call$2 (C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\sass\\sass.dart.js:101257:16)\n    at render_closure1.call$2 (C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\sass\\sass.dart.js:86329:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\sass\\sass.dart.js:30053:18)\n    at _FutureListener.handleError$1 (C:\\Users\\intel\\Desktop\\youtube\\realtime-pizza\\node_modules\\sass\\sass.dart.js:28582:21)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/scss/app.scss"]();
+/******/ 	
+/******/ })()
+;
